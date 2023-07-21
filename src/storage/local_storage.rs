cc6263f0a42d2044f4f8adc656e190886832658b
Alessandro <aleferrara1998@gmail.com>
2023-07-21 22:38:43 +0200

    migrate local storage to lru cache

--- src/storage/local_storage.rs
+++ src/storage/local_storage.rs
@@ -1,21 +1,23 @@
-use lrumap::{LruHashMap, Removed};
+use std::num::NonZeroUsize;
 
-pub struct LocalStorage {
-    storage: LruHashMap<i64, Vec<u8>>,   
+use lru::LruCache;
+
+pub struct LocalStorage<'a> {
+    storage: LruCache<&'a str, Vec<u8>>,   
 }
 
-impl LocalStorage {
+impl<'a> LocalStorage<'a> {
     pub fn new() -> Self {
         LocalStorage {
-            storage: LruHashMap::new(10),
+            storage: LruCache::new(NonZeroUsize::new(10).unwrap()),
         }
     }
 
-    pub fn get(&mut self, key: i64) -> Option<&Vec<u8>> {
-        self.storage.get(&key)
+    pub fn get(&mut self, key: &str) -> Option<&Vec<u8>> {
+        self.storage.get(key)
     }
 
-    pub fn set(&mut self, key: i64, value: Vec<u8>) -> Option<Removed<i64, Vec<u8>>> {
+    pub fn set(&mut self, key: &'a str, value: Vec<u8>) -> Option<(&str, Vec<u8>)> {
         self.storage.push(key, value)
     }
 
