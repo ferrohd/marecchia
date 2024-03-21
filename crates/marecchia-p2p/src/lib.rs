be244fbf75fb23c22e599fde4969181ff94c24db
Alessandro <aleferrara1998@gmail.com>
2024-03-21 17:11:41 +0100

    remove unused funcs

--- crates/marecchia-p2p/src/lib.rs
+++ crates/marecchia-p2p/src/lib.rs
@@ -1,15 +1,3 @@
-use p2p::client;
-use wasm_bindgen::prelude::*;
-
 mod http;
 mod loader;
 mod p2p;
-
-#[wasm_bindgen(start)]
-pub async fn start() -> Result<(), JsValue> {
-    tracing_wasm::set_as_global_default();
-
-    let mut client = client::new_p2p_client("idstream".to_string(), None).await.unwrap();
-
-    Ok(())
-}
