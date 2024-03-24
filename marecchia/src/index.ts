ab34cf2e945e675510c1123503d1eda53192a28d
Alessandro <aleferrara1998@gmail.com>
2024-03-24 12:51:23 +0100

    adapt to hls.js fragment loader type

--- marecchia/src/index.ts
+++ marecchia/src/index.ts
@@ -1,52 +1,54 @@
-import Hls, { FragmentLoaderContext, HlsConfig, LoadStats, Loader, LoaderCallbacks, LoaderConfiguration, LoaderContext, LoaderStats } from "hls.js";
+import Hls, { FragmentLoaderConstructor, FragmentLoaderContext, HlsConfig, LoadStats, Loader, LoaderCallbacks, LoaderConfiguration, LoaderContext, LoaderStats } from "hls.js";
 import { new_p2p_client, P2PClient } from "../../crates/marecchia-p2p/pkg/marecchia_p2p.js";
 
-export class P2PFragmentLoader implements Loader<FragmentLoaderContext> {
-    private p2pNetwork: P2PClient;
-    private httpLoader: (context: LoaderContext, config: LoaderConfiguration, callbacks: LoaderCallbacks<LoaderContext>) => void;
-    context: FragmentLoaderContext | null;
-    stats: LoaderStats;
+export function p2pFragmentLoader(stream_id: string): FragmentLoaderConstructor {
+    return class P2PFragmentLoader implements Loader<FragmentLoaderContext> {
+        private p2pNetwork: P2PClient;
+        private httpLoader: (context: LoaderContext, config: LoaderConfiguration, callbacks: LoaderCallbacks<LoaderContext>) => void;
+        context: FragmentLoaderContext | null;
+        stats: LoaderStats;
 
-    constructor(stream_id: string, confg: HlsConfig) {
-        this.p2pNetwork = new_p2p_client(stream_id);
-        this.httpLoader = new Hls.default.DefaultConfig.loader(confg).load;
-        this.stats = new LoadStats();
-        this.context = null;
-    }
-    load(context: FragmentLoaderContext, config: LoaderConfiguration, callbacks: LoaderCallbacks<FragmentLoaderContext>): void {
-        const segmentId = context.frag.sn.toString();
+        constructor(confg: HlsConfig) {
+            this.p2pNetwork = new_p2p_client(stream_id);
+            this.httpLoader = new Hls.default.DefaultConfig.loader(confg).load;
+            this.stats = new LoadStats();
+            this.context = null;
+        }
+        load(context: FragmentLoaderContext, config: LoaderConfiguration, callbacks: LoaderCallbacks<FragmentLoaderContext>): void {
+            const segmentId = context.frag.sn.toString();
 
-        // P2P exchanges only complete segments (no byte range support)
-        context.rangeStart = undefined;
-        context.rangeEnd = undefined;
+            // P2P exchanges only complete segments (no byte range support)
+            context.rangeStart = undefined;
+            context.rangeEnd = undefined;
 
-        this.p2pNetwork.request_segment(segmentId)
-            .then((segment) => {
-                callbacks.onSuccess({
-                    url: `p2p://${segmentId}`,
-                    data: segment,
-                }, this.stats, context, null);
-            })
-            .catch((_) => {
-                // Custom callbacks to upload a new segment once is downloaded from the server
-                const http_callbacks: LoaderCallbacks<FragmentLoaderContext> = {
-                    ...callbacks,
-                    onSuccess: (response, stats, context, networkDetails) => {
-                        if (response.data && response.data instanceof ArrayBuffer) {
-                            let data = new Uint8Array(response.data);
-                            this.p2pNetwork.send_segment(segmentId, data);
+            this.p2pNetwork.request_segment(segmentId)
+                .then((segment) => {
+                    callbacks.onSuccess({
+                        url: `p2p://${segmentId}`,
+                        data: segment,
+                    }, this.stats, context, null);
+                })
+                .catch((_) => {
+                    // Custom callbacks to upload a new segment once is downloaded from the server
+                    const http_callbacks: LoaderCallbacks<FragmentLoaderContext> = {
+                        ...callbacks,
+                        onSuccess: (response, stats, context, networkDetails) => {
+                            if (response.code && response.code === 200 && response.data && response.data instanceof ArrayBuffer) {
+                                let data = new Uint8Array(response.data);
+                                this.p2pNetwork.send_segment(segmentId, data);
+                            }
+                            callbacks.onSuccess(response, stats, context, networkDetails);
                         }
-                        callbacks.onSuccess(response, stats, context, networkDetails);
                     }
-                }
-                // Load the segment using the default HTTP loader
-                this.httpLoader(context, config, http_callbacks as LoaderCallbacks<LoaderContext>);
-            });
-    }
-    destroy(): void {
-        this.p2pNetwork.quit();
-    }
-    abort(): void {
-        this.destroy();
+                    // Load the segment using the default HTTP loader
+                    this.httpLoader(context, config, http_callbacks as LoaderCallbacks<LoaderContext>);
+                });
+        }
+        destroy(): void {
+            this.p2pNetwork.quit();
+        }
+        abort(): void {
+            this.destroy();
+        }
     }
 }
