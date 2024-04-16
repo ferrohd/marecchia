7fc60f1f50828aa1306bdcdd2bbbd5d60cb2e398
Alessandro <aleferrara1998@gmail.com>
2024-04-16 17:59:27 +0200

    wip tracker

--- crates/marecchia-tracker/src/main.rs
+++ crates/marecchia-tracker/src/main.rs
@@ -3,21 +3,22 @@ use libp2p::{
     identify,
     metrics::{Metrics, Recorder},
     multiaddr::Protocol,
-    noise, ping, rendezvous,
+    multihash::Multihash,
+    noise, ping, relay, rendezvous,
     swarm::{NetworkBehaviour, SwarmEvent},
     tcp, yamux, Multiaddr,
 };
-use libp2p_webrtc as webrtc;
-use prometheus_client::registry::Registry;
 use opentelemetry::KeyValue;
+use prometheus_client::registry::Registry;
+use sha3::{Digest, Sha3_512};
 use tokio::signal::unix::SignalKind;
 
-use std::time::Duration;
+use std::{default, hash::Hash, time::Duration};
 use std::{
     error::Error,
     net::{Ipv4Addr, Ipv6Addr},
 };
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Layer};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 mod metrics;
 
@@ -29,22 +30,29 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // Results in PeerID 12D3KooWDpJ7As7BWAwRMfu1VU2WCqNjvq387JEYKDBj4kx6nXTN which is
     // used as the rendezvous point by the other peer examples.
     let keypair = libp2p::identity::Keypair::ed25519_from_bytes([0; 32])?;
-    let rendezvous_id = keypair.public().to_peer_id();
+    let tracker_id = keypair.public().to_peer_id();
+
+    let digest = Sha3_512::digest(&tracker_id.to_bytes());
 
     let mut swarm = libp2p::SwarmBuilder::with_existing_identity(keypair)
         .with_tokio()
-        .with_tcp(
-            tcp::Config::default(),
-            noise::Config::new,
-            yamux::Config::default,
-        )?
+        //.with_tcp(
+        //    tcp::Config::default(),
+        //    noise::Config::new,
+        //    yamux::Config::default,
+        //)?
+        .with_other_transport(|key| {
+            let config = tcp::Config::new();
+            tcp::tokio::Transport::new(config)
+        })?
         .with_bandwidth_metrics(&mut metric_registry)
         .with_behaviour(|key| SwarmBehaviour {
             identify: identify::Behaviour::new(identify::Config::new(
-                "rendezvous-example/1.0.0".to_string(),
+                "rendezvous-example/0.0.1".to_string(),
                 key.public(),
             )),
             rendezvous: rendezvous::server::Behaviour::new(rendezvous::server::Config::default()),
+            relay: relay::Behaviour::new(tracker_id, relay::Config::default()),
             ping: ping::Behaviour::new(ping::Config::new().with_interval(Duration::from_secs(1))),
         })?
         .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(5)))
@@ -55,9 +63,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let listen_addr = Multiaddr::empty()
         .with(Protocol::Ip4(Ipv4Addr::UNSPECIFIED))
-        .with(Protocol::Ip6(Ipv6Addr::UNSPECIFIED))
-        .with(Protocol::Udp(0))
-        .with(Protocol::P2p(rendezvous_id));
+        //.with(Protocol::Ip6(Ipv6Addr::UNSPECIFpIED))
+        //.with(Protocol::Tcp(25565))
+        .with(Protocol::Udp(25565))
+        .with(Protocol::QuicV1);
+    //.with(Protocol::WebTransport);
+    //.with(Protocol::Certhash(Multihash::<64>::default()))
+    //.with(Protocol::P2p(rendezvous_id));
 
     swarm.listen_on(listen_addr)?;
 
@@ -107,6 +119,9 @@ async fn handle_behaviour_event(
         }
         ComposedSwarmEvent::Rendezvous(event) => {
             handle_rendezvous_event(swarm, event).await;
+        }
+        ComposedSwarmEvent::Relay(event) => {
+            handle_relay_event(swarm, event).await;
         }
         ComposedSwarmEvent::Ping(event) => {
             handle_ping_event(swarm, event).await;
@@ -178,6 +193,62 @@ async fn handle_rendezvous_event(
     }
 }
 
+async fn handle_relay_event(swarm: &mut libp2p::Swarm<SwarmBehaviour>, event: relay::Event) {
+    match event {
+        relay::Event::ReservationReqAccepted {
+            src_peer_id,
+            renewed,
+        } => {
+            tracing::info!(
+                "Reservation request accepted from {} (renewed: {})",
+                src_peer_id,
+                renewed
+            );
+        }
+        relay::Event::ReservationReqDenied { src_peer_id } => {
+            tracing::info!("Reservation request denied from {}", src_peer_id);
+        }
+        relay::Event::CircuitReqAccepted {
+            src_peer_id,
+            dst_peer_id,
+        } => {
+            tracing::info!(
+                "Circuit request accepted from {} to {}",
+                src_peer_id,
+                dst_peer_id
+            );
+        }
+        relay::Event::CircuitReqDenied {
+            src_peer_id,
+            dst_peer_id,
+        } => {
+            tracing::info!(
+                "Circuit request denied from {} to {}",
+                src_peer_id,
+                dst_peer_id
+            );
+        }
+        relay::Event::CircuitClosed {
+            src_peer_id,
+            dst_peer_id,
+            error,
+        } => {
+            tracing::info!(
+                "Circuit closed from {} to {}: {:?}",
+                src_peer_id,
+                dst_peer_id,
+                error
+            );
+        }
+        relay::Event::ReservationTimedOut { src_peer_id } => {
+            tracing::info!("Reservation timed out from {}", src_peer_id);
+        }
+        _ => {
+            tracing::info!("Received deprecated event: {:?}", event);
+        }
+    }
+}
+
 async fn handle_ping_event(swarm: &mut libp2p::Swarm<SwarmBehaviour>, event: ping::Event) {
     match event.result {
         Ok(duration) => {
@@ -195,6 +266,7 @@ async fn handle_ping_event(swarm: &mut libp2p::Swarm<SwarmBehaviour>, event: pin
 struct SwarmBehaviour {
     identify: identify::Behaviour,
     rendezvous: rendezvous::server::Behaviour,
+    relay: relay::Behaviour,
     ping: ping::Behaviour,
 }
 
@@ -202,6 +274,7 @@ struct SwarmBehaviour {
 enum ComposedSwarmEvent {
     Identify(identify::Event),
     Rendezvous(rendezvous::server::Event),
+    Relay(relay::Event),
     Ping(ping::Event),
 }
 
@@ -217,6 +290,12 @@ impl From<rendezvous::server::Event> for ComposedSwarmEvent {
     }
 }
 
+impl From<relay::Event> for ComposedSwarmEvent {
+    fn from(event: relay::Event) -> Self {
+        ComposedSwarmEvent::Relay(event)
+    }
+}
+
 impl From<ping::Event> for ComposedSwarmEvent {
     fn from(event: ping::Event) -> Self {
         ComposedSwarmEvent::Ping(event)
@@ -228,17 +307,21 @@ fn setup_tracing() -> Result<(), Box<dyn Error>> {
         .tracing()
         .with_exporter(opentelemetry_otlp::new_exporter().tonic())
         .with_trace_config(opentelemetry_sdk::trace::Config::default().with_resource(
-            opentelemetry_sdk::Resource::new(vec![KeyValue::new("service.name", "marecchia-tracker")]),
+            opentelemetry_sdk::Resource::new(vec![KeyValue::new(
+                "service.name",
+                "marecchia-tracker",
+            )]),
         ))
         .install_batch(opentelemetry_sdk::runtime::Tokio)?;
 
+    let filter_layer = EnvFilter::try_from_default_env().or_else(|_| EnvFilter::try_new("info"))?;
+    let fmt_layer = tracing_subscriber::fmt::layer();
+    let otel_layer = tracing_opentelemetry::layer().with_tracer(tracer);
+
     tracing_subscriber::registry()
-        .with(tracing_subscriber::fmt::layer().with_filter(EnvFilter::from_default_env()))
-        .with(
-            tracing_opentelemetry::layer()
-                .with_tracer(tracer)
-                .with_filter(EnvFilter::from_default_env()),
-        )
+        .with(filter_layer)
+        .with(fmt_layer)
+        .with(otel_layer)
         .try_init()?;
 
     Ok(())
